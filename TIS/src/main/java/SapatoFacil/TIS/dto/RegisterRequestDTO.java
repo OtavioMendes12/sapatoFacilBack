7780a58894c15a3a2d3b7f8efd3614ee6b033680
Otávio Mendes <102169915+OtavioMendes12@users.noreply.github.com>
2024-12-10 22:28:35 -0300

    Update RegisterRequestDTO.java

--- TIS/src/main/java/SapatoFacil/TIS/dto/RegisterRequestDTO.java
+++ TIS/src/main/java/SapatoFacil/TIS/dto/RegisterRequestDTO.java
@@ -2,5 +2,5 @@ package SapatoFacil.TIS.dto;
 
 import SapatoFacil.TIS.enuns.RoleEnum;
 
-public record RegisterRequestDTO (String nome, String cpf, String email, String senha, RoleEnum role)){
+public record RegisterRequestDTO (String nome, String cpf, String email, String senha, RoleEnum role){
 }
